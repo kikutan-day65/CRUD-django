98385365f5edc81fa5a82b8b41d4bcde25f43a75
Haruto Mori <baystarsman@outlook.jp>
2023-11-08 17:13:56 +0900

    funciton to show the list of stuff

--- crudproject/main/views.py
+++ crudproject/main/views.py
@@ -6,14 +6,19 @@ from .forms import ToDoForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/todo.html')
+    data = ToDoStuff.objects.all()
+
+    context = {'data': data}
+
+    return render(request, 'main/todo.html', context)
 
 
 def create_data(request):
     if request.method == 'POST':
         form = ToDoForm(request.POST)
         if form.is_valid():
-            print("POSTed succeesfully!")
+            data = form.save(commit=False)
+            data.save()
     else:
         form = ToDoForm()
     
