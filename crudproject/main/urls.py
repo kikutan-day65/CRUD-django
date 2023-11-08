a25c6dfb9a034bd17e5936f6fffde995a531f471
Haruto Mori <baystarsman@outlook.jp>
2023-11-08 17:00:00 +0900

    add to create data funciton and its url

--- crudproject/main/urls.py
+++ crudproject/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+
+    path('create_data/', views.create_data, name='create-data'),
 ]
--- crudproject/main/views.py
+++ crudproject/main/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
+from .models import ToDoStuff
+from .forms import ToDoForm
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/todo.html', name='home')
+    return render(request, 'main/todo.html')
+
+
+def create_data(request):
+    if request.method == 'POST':
+        form = ToDoForm(request.POST)
+        if form.is_valid():
+            print("POSTed succeesfully!")
+    else:
+        form = ToDoForm()
+    
+    context = {'form': form}
+
+    return render(request, 'main/form.html', context)
