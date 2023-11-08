06425b3ec232fdab48fe51c199f4696cb04eb5b9
Haruto Mori <baystarsman@outlook.jp>
2023-11-08 15:04:10 +0900

    add stuff detail data into database

--- crudproject/main/models.py
+++ crudproject/main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class ToDoStuff(models.Model):
     stuff = models.CharField(max_length=500)
+    detail = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.stuff
