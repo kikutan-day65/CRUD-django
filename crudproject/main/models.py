9f21da0c53d9b39b220151ffe4b0ad53b4bb7936
Haruto Mori <baystarsman@outlook.jp>
2023-11-08 14:50:28 +0900

    create database model

--- crudproject/main/models.py
+++ crudproject/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ToDoStuff(models.Model):
+    stuff = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.stuff
+    
